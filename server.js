7ce154654b5d6d59a7c170282f6524479bcfb272
sollertis8 <mizumedia@gmail.com>
2017-11-26 18:04:17 -0500

    updated config and server files
    updated config and server files

--- server.js
+++ server.js
@@ -1,60 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-// const morgan = require('morgan');
-// const bodyParser = require('body-parser');
-
-// const BlogPosts = require('./models');
-
-// const jsonParser = bodyParser.json();
-// const app = express();
-
-// const blogPostRouter = require('./blogPostRouter');
-
-// // log the http layer
-// app.use(morgan('common'));
-
-// app.use(express.static('public'));
-
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + '/views/index.html');
-// });
-
-// app.use('/blog-posts', blogPostRouter);
-
-// let server;
-
-// function runServer() {
-//   const port = process.env.PORT || 8080;
-//   return new Promise((resolve, reject) => {
-//     server = app.listen(port, () => {
-//       console.log(`Your app is listening on port ${port}`);
-//       resolve(server);
-//     }).on('error', err => {
-//       reject(err)
-//     });
-//   });
-// }
-
-// function closeServer() {
-//   return new Promise((resolve, reject) => {
-//     console.log('Closing server');
-//     server.close(err => {
-//       if (err) {
-//         reject(err);
-//         // so we don't also call `resolve()`
-//         return;
-//       }
-//       resolve();
-//     });
-//   });
-// }
-
-// if (require.main === module) {
-//   runServer().catch(err => console.error(err));
-// };
-
-// module.exports = {app, runServer, closeServer};
-
 const bodyParser = require('body-parser');
 const express = require('express');
 const morgan = require('morgan');
@@ -115,6 +58,8 @@ app.post('/posts', (req, res) => {
       console.error(err);
       res.status(500).json({error: 'Something went wrong'});
   });
+
+});
 
   app.delete('/posts/:id', (req, res) => {
     BlogPost
@@ -202,5 +147,61 @@ if (require.main === module) {
 
 module.exports = {runServer, app, closeServer};
 
+// const express = require('express');
+// const router = express.Router();
+// const morgan = require('morgan');
+// const bodyParser = require('body-parser');
 
-})
+// const BlogPosts = require('./models');
+
+// const jsonParser = bodyParser.json();
+// const app = express();
+
+// const blogPostRouter = require('./blogPostRouter');
+
+// // log the http layer
+// app.use(morgan('common'));
+
+// app.use(express.static('public'));
+
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/views/index.html');
+// });
+
+// app.use('/blog-posts', blogPostRouter);
+
+// let server;
+
+// function runServer() {
+//   const port = process.env.PORT || 8080;
+//   return new Promise((resolve, reject) => {
+//     server = app.listen(port, () => {
+//       console.log(`Your app is listening on port ${port}`);
+//       resolve(server);
+//     }).on('error', err => {
+//       reject(err)
+//     });
+//   });
+// }
+
+// function closeServer() {
+//   return new Promise((resolve, reject) => {
+//     console.log('Closing server');
+//     server.close(err => {
+//       if (err) {
+//         reject(err);
+//         // so we don't also call `resolve()`
+//         return;
+//       }
+//       resolve();
+//     });
+//   });
+// }
+
+// if (require.main === module) {
+//   runServer().catch(err => console.error(err));
+// };
+
+// module.exports = {app, runServer, closeServer};
+
+
